326269f4c7429be6729247010945df9e371ef2d0
Vinoth Kumar Mohanraj <vmohanraj@devx.systems>
2022-08-08 11:36:04 +0530

    Solace H2 Two Phase Commit implemented

--- src/main/java/com/example/tpc/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/tpc/service/impl/EmployeeServiceImpl.java
@@ -5,12 +5,14 @@ import com.example.tpc.repository.EmployeeRepository;
 import com.example.tpc.service.EmployeeService;
 import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Log
 public class EmployeeServiceImpl implements EmployeeService {
 
   @Autowired
@@ -30,8 +32,15 @@ public class EmployeeServiceImpl implements EmployeeService {
   @Override
   @Transactional(rollbackOn = Exception.class)
   public void save(final Employee employee, final boolean fail) throws Exception {
+    log.info("Sending JMS message to Solace Message Broker..." + employee.getName());
     jmsTemplate.convertAndSend(queueName, employee.getName());
+
+    log.info("Persisting employee data to H2 database...");
     employeeRepository.save(employee);
-    if(fail) throw new Exception("Exception occurred. Transaction Rollback should happen");
+
+    if(fail) {
+      log.info("Validating distributed transaction rollback...");
+      throw new Exception("Exception occurred. Transaction Rollback should happen");
+    }
   }
 }
